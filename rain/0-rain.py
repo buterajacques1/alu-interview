33136f95104ee648a9137d64bab5461cc360c3b7
buterajacques1 <jbutera702@gmail.com>
2023-02-24 01:05:13 +0200

    RAIN

--- rain/0-rain.py
+++ rain/0-rain.py
@@ -1,13 +1,14 @@
-#!/usr/bin/python3
-"""Given a list of non-negative integers
-representing the heights of walls with unit width 1
-as if viewing the cross-section of a relief map
-calculate how many square units of water will be retained after it rains.
-"""
-
-
 def rain(walls):
-    """Calculating the square units of retained water"""
+    """Calculate how many square units of water will be retained after it rains.
+
+    Args:
+        walls (List[int]): A list of non-negative integers representing the
+            heights of walls with unit width 1.
+
+    Returns:
+        int: The total amount of rainwater retained.
+
+    """
     if not walls:
         return 0
 
@@ -15,14 +16,17 @@ def rain(walls):
     left = [0] * n
     right = [0] * n
 
+    # Calculate the maximum height of walls to the left of each position.
     left[0] = walls[0]
     for i in range(1, n):
         left[i] = max(left[i - 1], walls[i])
 
+    # Calculate the maximum height of walls to the right of each position.
     right[n - 1] = walls[n - 1]
     for i in range(n - 2, -1, -1):
         right[i] = max(right[i + 1], walls[i])
 
+    # Calculate the amount of water retained at each position.
     water = 0
     for i in range(n):
         water += min(left[i], right[i]) - walls[i]
