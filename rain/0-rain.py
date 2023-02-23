6a9c5c32d91fd879182187905bb4ab1ce3388e1e
buterajacques1 <jbutera702@gmail.com>
2023-02-24 01:15:59 +0200

    rain 1

--- rain/0-rain.py
+++ rain/0-rain.py
@@ -1,41 +1,19 @@
 #!/usr/bin/python3
 def rain(walls):
-    """Calculate how much rainwater is retained in a given list of walls.
+    n = len(walls)
+    left = [0] * n
+    right = [0] * n
 
-    Args:
-        walls (List[int]): A list of non-negative integers representing the
-            heights of walls with unit width 1.
+    left[0] = walls[0]
+    for i in range(1, n):
+        left[i] = max(left[i - 1], walls[i])
 
-    Returns:
-        int: The total amount of rainwater retained.
+    right[n - 1] = walls[n - 1]
+    for i in range(n - 2, -1, -1):
+        right[i] = max(right[i + 1], walls[i])
 
-    """
-    if not walls:
-        return 0
+    water = 0
+    for i in range(n):
+        water += min(left[i], right[i]) - walls[i]
 
-    size = len(walls) - 1
-    prev = walls[0]
-    index = 0
-    aux = 0
-    water_retained = 0
-
-    for i in range(1, size + 1):
-        if walls[i] >= prev:
-            aux = 0
-            index = i
-            prev = walls[i]
-        else:
-            aux += prev - walls[i]
-            water_retained += prev - walls[i]
-
-    if index < size:
-        prev = walls[i]
-        water_retained -= aux
-
-        for i in range(size, index - 1, -1):
-            if walls[i] >= prev:
-                prev = walls[i]
-            else:
-                water_retained += prev - walls[i]
-
-    return water_retained
+    return water
